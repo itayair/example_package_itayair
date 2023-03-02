a8fadb9a4057cfeed534fd11d8ae2eb055f0df31
iy245 <iy24592@gmail.com>
2023-03-02 13:22:46 +0200

    check

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setuptools.setup(
     name = "example_package_itayair",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Itay Yair",
     author_email = "iy24592@gmail.com",
     description = "A simple Python package",
